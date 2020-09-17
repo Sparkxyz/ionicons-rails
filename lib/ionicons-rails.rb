efa0a8f6d049ab1460df6b8822368b6464b8b975
Abe Park <7087312+abepark01@users.noreply.github.com>
2020-09-17 14:43:00 -0700

    fix sprockets 3 deprecations

--- lib/ionicons-rails.rb
+++ lib/ionicons-rails.rb
@@ -1,8 +1,13 @@
+# frozen_string_literal: true
+
 module Ionicons
   module Rails
     class Engine < ::Rails::Engine
       initializer "ionicons-rails.assets.precompile" do |app|
-        app.config.assets.precompile << %r(ionicons\.(?:eot|svg|ttf|woff)$)
+        app.config.assets.precompile << 'ionicons.eot'
+        app.config.assets.precompile << 'ionicons.svg'
+        app.config.assets.precompile << 'ionicons.ttf'
+        app.config.assets.precompile << 'ionicons.woff'
       end
     end
   end
