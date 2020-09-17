11569c2c0ce5309340fbaa4e974c9944bf66eb49
Abe Park <7087312+abepark01@users.noreply.github.com>
2020-09-17 16:03:19 -0700

    support sprockets 4

--- lib/ionicons-rails.rb
+++ lib/ionicons-rails.rb
@@ -2,7 +2,8 @@ module Ionicons
   module Rails
     class Engine < ::Rails::Engine
       initializer "ionicons-rails.assets.precompile" do |app|
-        app.config.assets.precompile << %r(ionicons\.(?:eot|svg|ttf|woff)$)
+        app.config.assets.paths << app.root.join('vendor', 'assets', 'fonts')
+        app.config.assets.precompile << %w[ionicons.svg ionicons.eot ionicons.woff ionicons.ttf]
       end
     end
   end
