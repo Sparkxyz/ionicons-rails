9a93f5d6a58676da993586c16da89c92b4e95325
Abe Park <7087312+abepark01@users.noreply.github.com>
2020-09-17 17:19:35 -0700

    add asset paths

--- lib/ionicons-rails.rb
+++ lib/ionicons-rails.rb
@@ -1,8 +1,11 @@
 module Ionicons
   module Rails
     class Engine < ::Rails::Engine
-      initializer "ionicons-rails.assets.precompile" do |app|
-        app.config.assets.paths << app.root.join('vendor', 'assets', 'fonts')
+      initializer 'ionicons-rails.assets' do |app|
+        app.config.assets.paths << root.join('vendor', 'assets', 'fonts')
+      end
+
+      initializer 'ionicons-rails.assets.precompile' do |app|
         app.config.assets.precompile << %w[ionicons.svg ionicons.eot ionicons.woff ionicons.ttf]
       end
     end
