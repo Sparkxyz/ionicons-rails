a58e6e2b0290ec20f558428aa5198f8dc06e4b18
Abe Park <7087312+abepark01@users.noreply.github.com>
2020-09-17 17:37:38 -0700

    debug

--- lib/ionicons-rails.rb
+++ lib/ionicons-rails.rb
@@ -1,11 +1,9 @@
 module Ionicons
   module Rails
     class Engine < ::Rails::Engine
-      initializer 'ionicons-rails.assets' do |app|
-        app.config.assets.paths << root.join('vendor', 'assets', 'fonts')
-      end
-
       initializer 'ionicons-rails.assets.precompile' do |app|
+        puts 'GOT HERE ionicons-rails config'
+        app.config.assets.paths << root.join('vendor', 'assets', 'fonts')
         app.config.assets.precompile << %w[ionicons.svg ionicons.eot ionicons.woff ionicons.ttf]
       end
     end
